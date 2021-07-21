65e29df1fd7667222a3fe9600dbe16fa60acbd72
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-07-21 13:04:13 -0700

    Completed Flow Lab

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 199) {
+    return 'This one is on me!'
+  } else if (ride <= 2001) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (ride >= 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(place){
+  return ternaryCheckCity = place === "NYC"? 'Ok, sounds good.' : 'No go.'
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+    return 'Thank you so much.';
+    case 'not as generous':
+    return 'Thank you.';
+    default: 
+    return 'Bye.';
+  }
+  
 }
